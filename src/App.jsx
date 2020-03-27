a3a69189099239dcab852ff0a3d15017da79a7ee
Truong Viet Trung <viettrung9012@yahoo.com>
2020-03-27 21:16:46 +0800

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+﻿import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Blank from "./components/Blank";
+
+import Master_Detail from "./components/Master_Detail";
+
+import List from "./components/List";
+
+import Grid from "./components/Grid";
+
+//TODO Web Template Studio: Add routes for your new pages here.
+const App = () => {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = { Blank } />
+          <Route path = "/Master_Detail" component = { Master_Detail } />
+          <Route path = "/List" component = { List } />
+          <Route path = "/Grid" component = { Grid } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+}
+
+export default App;
