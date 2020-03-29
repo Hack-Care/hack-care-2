0f9b10fca015ddd508e4649db6517a00d9fc233b
= <liliwei25@hotmail.com>
2020-03-29 15:28:07 +0800

    add apollo client

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,39 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { setContext } from 'apollo-link-context';
+import { HttpLink } from "apollo-link-http";
+import {ApolloProvider} from "@apollo/react-hooks";
+import {BrowserRouter} from "react-router-dom";
+import Cookies from 'js-cookie';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: "http://localhost:3001/graphql"
+});
+const authLink = setContext((_, { headers }) => {
+    const token = Cookies.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ""
+        }
+    }
+});
+const client = new ApolloClient({
+    cache,
+    link: authLink.concat(link),
+});
 
 ReactDOM.render(
     <BrowserRouter>
-      <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
     </BrowserRouter>,
   document.getElementById("root")
 );
